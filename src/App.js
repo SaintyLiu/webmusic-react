5358d50ee2e1466f53f3e72b60e2ec6a37c686f8
SaintyLiu <541491286@qq.com>
2021-08-16 08:34:37 +0800

    commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 
+import React from 'react';
+
+import Top from './components/Top';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div>
+      <Top/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Content/index.jsx
@@ -0,0 +1,44 @@
+import React,{Component } from 'react';
+import {Input} from 'antd';
+import {SearchOutlined} from '@ant-design/icons';
+
+export default class Index extends Component{
+  goToSearchPage = (e) => {
+    window.open(`#/search/${e.target.value}`, '_self')
+  }
+
+  render(){
+    return (
+      <div className="top-container">
+        <div className="left-box">
+          <div className="icon-wrapper">
+            <span
+            onClick={() => window.history.go('/')}
+            className="iconfont icon-home"
+            ></span>
+            <span className="iconfont icon-sami-select"></span>
+            <span className="iconfont icon-full-screen"></span>
+          </div>
+          <div className="history-wrapper"> 
+            <span
+            onClick={() => window.history.go(-1)}
+            className="iconfont icon-arrow-lift"
+            ></span>
+            <span
+            onClick={() => window.history.go(1)}
+            className="iconfont icon-arrow-right"
+            ></span>
+          </div>
+        </div>
+        <div className="right-box">
+          <Input
+            size="middle"
+            prefix={<SearchOutlined />}
+            placeholder="搜索"
+            onPressEnter={this.goToSearchPage}
+          ></Input>
+        </div>
+      </div>
+    )
+  }
+}
