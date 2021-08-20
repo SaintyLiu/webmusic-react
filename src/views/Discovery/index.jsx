9b92dfb23d66ad652c90a65e7a5b3f03fed0c4f4
SaintyLiu <541491286@qq.com>
2021-08-20 20:34:41 +0800

    完成discovery部分

--- src/views/Discovery/index.jsx
+++ src/views/Discovery/index.jsx
@@ -14,7 +14,7 @@ export default class Index extends Component {
 
     this.state = {
       // 轮播图
-      banner: [],
+      banners: [],
       // 推荐歌单
       recommendList: [],
       // 新歌
@@ -33,54 +33,54 @@ export default class Index extends Component {
     this.getMVData();
   }
 
-  async getBannersData(){
-    const res = await getBannerList()
-    
-    if (res.data.code === 200){
+  async getBannersData() {
+    const res = await getBannerList();
+
+    if (res.data.code === 200) {
       this.setState({
         banners: res.data.banners,
-      })
+      });
     }
   }
 
   async getRecommendListData() {
-    const res = await getRecommendList()
+    const res = await getRecommendList();
 
     if (res.data.code === 200) {
       this.setState({
         recommendList: res.data.result,
-      })
+      });
     }
   }
 
-  async getNewSongData(){
-    const res = await getNewSong()
+  async getNewSongData() {
+    const res = await getNewSong();
 
     if (res.data.code === 200) {
       this.setState({
-        newsong: res.data.result,
-      })
+        newSong: res.data.result,
+      });
     }
   }
 
-  async getMVData(){
-    const res = await getMv()
+  async getMVData() {
+    const res = await getMv();
 
-    if (res.data.code === 200){
+    if (res.data.code === 200) {
       this.setState({
         mv: res.data.result,
-      })
+      });
     }
   }
 
   renderSwiper = () => {
-    const { banners } = this.state
+    const { banners } = this.state;
     const setting = {
-      className :'center',
-      infinite : true,
-      speed : 500,
-      autoplay : true,
-    }
+      className: "center",
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+    };
     return (
       <Slider {...setting}>
         {banners.map((item) => {
@@ -88,15 +88,15 @@ export default class Index extends Component {
             <div key={item.targetId}>
               <img src={item.imageUrl} alt="" />
             </div>
-          )
+          );
         })}
       </Slider>
-    )
-  }
+    );
+  };
 
   //推荐歌单
   renderRecommend = () => {
-    const { recommendList } = this.state
+    const { recommendList } = this.state;
     return (
       <div className="recommend">
         <h3 className="title">推荐歌单</h3>
@@ -107,23 +107,105 @@ export default class Index extends Component {
                 <div
                   className="img-wrapper"
                   onClick={() => this.toPlayList(item.id)}
-                  >
+                >
                   <div className="desc-wrapper">
                     <span className="desc">{item.copywriter}</span>
                   </div>
                   <img src={item.picUrl} alt="" />
                   <span className="iconfont icon-play"></span>
                 </div>
-                  <p className="name">{item.name}</p>
+                <p className="name">{item.name}</p>
               </div>
-            )
+            );
           })}
         </div>
       </div>
-    )
-  }
+    );
+  };
+
+  //最新音乐
+  renderNewSong = () => {
+    const { newSong } = this.state;
+    return (
+      <div className="news">
+        <h3 className="title">最新音乐</h3>
+        <div className="items">
+          {newSong.map((item) => {
+            return (
+              <div className="item" key={item.id}>
+                <div className="img-wrap">
+                  <img src={item.picUrl} alt="" />
+                  <span
+                    onClick={() => this.playMusic(item.id)}
+                    className="iconfont icon-play"
+                  ></span>
+                </div>
+                <div className="song-wrap">
+                  <div className="song-name">{item.name}</div>
+                  <div className="singer">{item.song.artists[0].name}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  //推荐Mv
+  renderMv = () => {
+    const { mv } = this.state;
+    return (
+      <div className="mvs">
+        <h3 className="title">推荐MV</h3>
+        <div className="items">
+          {mv.map((item) => {
+            return (
+              <div className="item" key={item.id}>
+                <div className="img-wrap" onClick={() => this.toMv(item.id)}>
+                  <img src={item.picUrl} alt="" />
+                  <span className="iconfont icon-play"></span>
+                  <div className="num-wrap">
+                    <div className="iconfont icon-play"></div>
+                    <div className="num">{item.playCount}</div>
+                  </div>
+                </div>
+                <div className="info-wrap">
+                  <div className="name">{item.copywriter}</div>
+                  <div className="singer">{item.artistName}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  toPlayList = (id) => {
+    this.props.history.push(`/playlist/${id}`);
+  };
+
+  playMusic = (id) => {
+    bus.emit("playMusic", id);
+  };
+
+  toMv = (id) => {
+    this.props.history.push(`/mv/${id}`);
+  };
 
   render() {
-    return <div>发现音乐</div>;
+    return (
+      <div className="discovery-container">
+        {/* 渲染轮播图 */}
+        {this.renderSwiper()}
+        {/* 渲染推荐歌单 */}
+        {this.renderRecommend()}
+        {/* 渲染最新音乐 */}
+        {this.renderNewSong()}
+        {/* 渲染推荐MV */}
+        {this.renderMv()}
+      </div>
+    );
   }
 }
